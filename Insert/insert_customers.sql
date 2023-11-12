bc2464fb2922c1b6fa391a7f31f3046ba4cca6e0
deknaa <74857225+deknaa@users.noreply.github.com>
2023-11-12 13:07:02 +0800

    Added Insert Query

---
+++ Insert/insert_customers.sql
@@ -0,0 +1,13 @@
+-- Query Insert data ke table Customers
+
+INSERT INTO customers (name_customer, email_customer, address_customer, dob_customer, age_customer) VALUES 
+('Putu Arya', 'putuarya@gmail.com', 'Jalan Gunung Batur', '1990-01-10', 32),
+('Ketut Bagus', 'ketutbagus@gmail.com', 'Jalan Gunung Agung', '2000-05-16', 23),
+('Ahmad Junaedi', 'ahmad@gmail.com', 'Jalan Tangkuban Perahu', '1993-07-19', 29),
+('Danu Irawan', 'danuirawan@gmail.com', 'Jalan Raya Canggu', '1991-02-10', 31),
+('Bagus Maulidan', 'bagusmaulidan@gmail.com', 'Jalan Raya Abiansemal', '2002-01-10', 21),
+('Kadek Dewa', 'dewak@gmail.com', 'Jalan Raya Dalung', '2003-10-16', 20),
+('Guntur', 'guntur@gmail.com', 'Jalan Raya Jimbaran', '1995-07-25', 27),
+('Ahmad Syahroni', 'ahmad@gmail.com', 'Jalan Bay Pass Ngurah Rai', '2000-06-13', 23),
+('Muhammad Prayogi', 'yogi@gmail.com', 'Jalan Abian Kapas', '1990-03-01', 32),
+('Ahmad Jeka', 'ahmadjeka@gmail.com', 'Jalan Batu', '2000-12-16', 23)
---
+++ Insert/insert_products.sql
@@ -0,0 +1,13 @@
+-- Query Insert data ke table Products
+
+INSERT INTO products (name_product, price_product, stock_product, type_product) VALUES
+('TV LED LG 32 Inch', 3600000, 15, 'Electronic'),
+('TV LED Samsung 24 Inch', 2500000, 32, 'Electronic'),
+('TV LED Xiaomi 32 Inch', 3000000, 50, 'Electronic'),
+('HP iPhone 14 Pro Max', 19000000, 23, 'Electronic'),
+('HP Samsung A3s', 2500000, 100, 'Electronic'),
+('Magic Jar Sharp', 750000, 150, 'Electronic'),
+('Air Cooler Sharp', 1950000, 30, 'Electronic'),
+('Sound System Sony', 5000000, 75, 'Electronic'),
+('Laptop Lenovo LOQ Gaming ', 13999999, 200, 'Electronic'),
+('Laptop Asus ROG Strix', 23999999, 50, 'Electronic')
